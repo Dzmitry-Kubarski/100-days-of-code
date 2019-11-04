39181fb81b5b3c662b8c2b71220142667e954250
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2019-11-04 21:46:33 +0300

    День 20, итоги
    #100DaysOfCode
    
    Продолжаю практиковать вёрстку.

--- layout 5/js/main.js
+++ layout 5/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 
-    $('.offer__inner').slick();
+    $('.offer__inner').slick({
+        infinite: false,
+        prevArrow: '<button class="slick-prev slick-btn"></button>',
+        nextArrow: '<button class="slick-next slick-btn"></button>',
+
+    });
+
+    $('select').styler();
 
 });
