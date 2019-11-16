c779be0b6163c7927e9695d729d669fc0fe359e2
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2019-11-16 21:04:46 +0300

    День 31, итоги
    #100DaysOfCode
    
    Закончил вёрстку очередного макета.

---
+++ projects/layout-7/src/assets/js/app.js
@@ -0,0 +1,21 @@
+$(function () {
+
+    $('#headerMenu').on('click', function () {
+        $('#menuList').slideToggle();
+    });
+
+    // Scroll
+
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+
+        var elementID = $(this).data('scroll');
+        var elementOffset = $(elementID).offset().top;
+
+        $('html, body').animate({
+            scrollTop: elementOffset
+        });
+    });
+
+});
+
