e26ad0648c9afc01a7e2d22fb687e9ce9fc13763
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2019-11-17 20:58:04 +0300

    День 32, итоги
    #100DaysOfCode

--- projects/layout-7/src/assets/js/app.js
+++ projects/layout-7/src/assets/js/app.js
@@ -1,20 +1,45 @@
+//= ../../../../../node_modules/slick-carousel/slick/slick.js
+
+
 $(function () {
 
     $('#headerMenu').on('click', function () {
-        $('#menuList').slideToggle();
+        $('#menuList').slideToggle(300, function () {
+
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+
+        });
+
+
     });
+
 
     // Scroll
 
     $('[data-scroll]').on('click', function (event) {
         event.preventDefault();
 
-        var elementID = $(this).data('scroll');
-        var elementOffset = $(elementID).offset().top;
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
 
         $('html, body').animate({
             scrollTop: elementOffset
         });
+    });
+
+
+    // Slider Slick
+
+    $('.installation__slider').slick({
+        dots: true,
+        dotsClass: "my-dots",
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 800,
+
     });
 
 });
