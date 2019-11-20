88e5b242080b6b6fcd4b37ff600b3e804504d6f9
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2019-11-20 23:56:49 +0300

    День 35
    #100DaysOfCode
    
    Кода было не много в последние дни, погрузился в теорию.

--- projects/layout-7/src/assets/js/app.js
+++ projects/layout-7/src/assets/js/app.js
@@ -1,4 +1,5 @@
 //= ../../../../../node_modules/slick-carousel/slick/slick.js
+//= components/jquery.parallax-0.2.js
 
 
 
@@ -15,6 +16,7 @@ $(function () {
 
 
     // Scroll
+    // ======
 
     $('[data-scroll]').on('click', function (event) {
         event.preventDefault();
@@ -28,7 +30,18 @@ $(function () {
     });
 
 
+    // ----- Cкрол к секции -----
+
+    $('.menu__link').click(function () {
+
+        var target = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+        return false;
+    });
+
+
     // Slider Slick
+    // ============
 
     $('.installation__slider').slick({
         dots: true,
@@ -37,8 +50,58 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 800,
-
     });
+
+
+    // Parallax Move
+    // =============
+
+    // if ($(window).width() > 960) {
+    //     $('body').parallax({
+    //         'elements': [
+    //             {
+    //                 'selector': '.js-move',
+    //                 'properties': {
+    //                     'x': {
+    //                         'left': {
+    //                             'initial': 475,
+    //                             'multiplier': 0.02,
+    //                             'unit': 'px',
+    //                             'invert': false
+    //                         }
+    //                     },
+    //                     'y': {
+    //                         'top': {
+    //                             'initial': 425,
+    //                             'multiplier': 0.03,
+    //                             'unit': 'px',
+    //                             'invert': false
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         ]
+    //     });
+    // } else {
+    //     // change functionality for larger screens
+    // }
+
+    // Parallax Move
+    // =============
+
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene, {
+    });
+
+    // $(function () {
+    //     $(document).on('mousemove', function (e) {
+    //         $('.js-move').css({
+    //             left: e.pageX / 10 + 100,
+    //             top: e.pageY / 10 + 100
+    //         });
+
+    //     });
+    // });
 
 });
 
