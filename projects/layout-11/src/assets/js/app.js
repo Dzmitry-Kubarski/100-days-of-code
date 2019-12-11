41a7fd6dae8950727db776f08a7435b1f64c0e3e
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2019-12-11 21:36:44 +0300

    День 56
    #100DaysOfCode
    Продолжаю верстать многостраничный сайт-блог.

--- projects/layout-11/src/assets/js/app.js
+++ projects/layout-11/src/assets/js/app.js
@@ -46,6 +46,40 @@ $(function () {
         // nextArrow: '<button class="slider-btn_next"><img src="assets/img/next.svg" alt="arrow"></button>',
         // prevArrow: '<button class="slider-btn_prev"><img src="assets/img/prev.svg" alt="arrow"></button>',
 
+        // ----- responsive slider -----
+        responsive: [
+            {
+                breakpoint: 890,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    prevArrow: false,
+                    nextArrow: false,
+                    dots: true,
+                    centerMode: false,
+                }
+            },
+
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    prevArrow: false,
+                    nextArrow: false,
+                    dots: true,
+                    centerMode: false,
+                }
+            },
+
+        ]
+
+    });
+
+
+    $('#btnShow').click(function (event) {
+        event.preventDefault();
+        $('.articles__row').append('<article class="post"><div class="post__inner"><span class="post__subtitle">TRAVEL</span><span class="post__dash">/</span><span class="post__date">March 6, 2019</span><h2 class="post__title">Make Your Adventure Sound REALLY Epic</h2><div class="post__img-inner"><a class="post__img-link" href="#"><img src="assets/img/articles/img-2.jpg" alt=""></a></div><p class="post__text">Do you find yourself reading stories about adventures in outdoor magazines and websites and feeling down about what you did on your summer vacation?</p><button class="btn" type="submit">Read More</button></div> <!-- /post__inner --></article>');
     });
 
 });
