9b799ea922e88a73d4f932d24dea08ddf1198e10
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-01-25 00:34:12 +0300

    Вёрстка, макет №15.

--- projects/layout-15/src/assets/js/app.js
+++ projects/layout-15/src/assets/js/app.js
@@ -3,9 +3,73 @@
 /* Slick slider */
 //= ../../../../../node_modules/slick-carousel/slick/slick.js
 
+/* Slick slider */
+//= vendor/jquery.downCount.js
+
 
 $(function () {
 
     $(".single_image").fancybox();
+
+
+    $('.finish__tabs-bnt').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.finish__items').find('.finish__item').removeClass('active-item').hide();
+        $('.finish__tabs').find('.finish__tabs-bnt').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-item').fadeIn();
+        return false;
+    });
+
+
+    //------- Таймер ---------
+    function e() {
+        var e = new Date;
+        e.setDate(e.getDate() + 3);
+
+        var dd = e.getDate();
+        var mm = e.getMonth() + 1;
+        var y = e.getFullYear();
+
+        var futureFormattedDate = mm + "/" + dd + "/" + y + ' 12:00:00';
+
+        return futureFormattedDate;
+    }
+
+    $('.countdown').downCount({
+        date: e(),
+        offset: -4
+    }, function () {
+        alert('WOOT WOOT, done!');
+    });
+
+
+    // ------------ Tabs Variants -------
+    $('.variants__dots-link').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.variants__content').removeClass('active').hide();
+        $('.variants__dots').find('.variants__dots-link').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active').fadeIn();
+        return false;
+    });
+
+
+    // -------- Popup --------
+    $('[data-popup]').click(function () {
+        $('body').addClass('over-hide');
+        $('.popup__wrap').addClass('active');
+        $('.popup').removeClass('active');
+        $('.' + $(this).data('popup')).addClass('active');
+
+    });
+
+    $('.popupBtn, .popup__close, .popup__overlay').click(function () {
+        $('.popup__wrap').toggleClass('active');
+        $('.fixedHeader').toggleClass('active');
+        $('body').removeClass('over-hide');
+        // $('[data-wrap-form]').removeClass('hidden');
+        // $('[data-wrap-sent]').addClass('hidden');
+    });
 
 });
