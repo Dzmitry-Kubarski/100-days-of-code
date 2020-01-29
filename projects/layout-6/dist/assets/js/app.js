22bcacbdcda145d82ddd9216e483c374935f7087
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-01-29 22:20:12 +0300

    Правки по вёрстке, портфолио

--- projects/layout-6/dist/assets/js/app.js
+++ projects/layout-6/dist/assets/js/app.js
@@ -2,13 +2,18 @@
 
 $(function () {
 
-
-    $(function () {
-
-        $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-
+    // Active link
+    document.querySelectorAll('.nav li a').forEach(function (el) {
+        if (window.location.pathname.indexOf(el.getAttribute('href')) > -1) {
+            el.classList.add('active');
+        }
     });
 
+
+    // Button mobail
+    $(".m_button").click(function () {
+        $('body').toggleClass("active");
+    });
 
 
     // ----- Слайдер Slick -----
@@ -55,13 +60,24 @@ $(function () {
     $('.portfolio__dots-btn').on('click', function (event) {
         event.preventDefault();
         var id = $(this).attr('data-id');
+        var target = $(this).attr('href');
+
         $('.portfolio__slide').removeClass('active').hide();
         $('.portfolio__dots').find('.portfolio__dots-btn').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active').fadeIn();
+
+        $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+
         return false;
     });
 
 
+    // $('.header__btn, .nav__link--mobail, .nav__link').click(function () {
+
+    //     var target = $(this).attr('href');
+    //     $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+    //     return false;
+    // });
 
 });
--- projects/layout-6/src/assets/js/app.js
+++ projects/layout-6/src/assets/js/app.js
@@ -2,66 +2,33 @@
 
 $(function () {
 
-
-    $(function () {
-
-        $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-
+    // Active link
+    document.querySelectorAll('.nav li a').forEach(function (el) {
+        if (window.location.pathname.indexOf(el.getAttribute('href')) > -1) {
+            el.classList.add('active');
+        }
     });
 
 
-
-    // ----- Слайдер Slick -----
-
-    // $('.portfolio__slider').slick({
-    //     dots: false,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 1,
-    //     prevArrow: '<button class="product__slider-btn_prev product__slider-btn"><img src="img/btn-prev.svg" alt="arrow"></button>',
-    //     nextArrow: '<button class="product__slider-btn_next product__slider-btn"><img src="img/btn-next.svg" alt="arrow"></button>',
-
-
-    //     // ----- responsive slider -----
-    //     responsive: [
-
-    //         {
-    //             breakpoint: 1240,
-    //             settings: {
-    //                 prevArrow: false,
-    //                 nextArrow: false,
-    //                 dots: true,
-
-    //             }
-    //         },
-
-    //         {
-    //             breakpoint: 567,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1,
-    //                 prevArrow: false,
-    //                 nextArrow: false,
-    //                 dots: true,
-    //                 centerMode: false,
-
-    //             }
-    //         },
-    //     ]
-    // });
-
-
+    // Button mobail
+    $(".m_button").click(function () {
+        $('body').toggleClass("active");
+    });
 
     // ------------ Dots -------
     $('.portfolio__dots-btn').on('click', function (event) {
         event.preventDefault();
         var id = $(this).attr('data-id');
+        var target = $(this).attr('href');
+
         $('.portfolio__slide').removeClass('active').hide();
         $('.portfolio__dots').find('.portfolio__dots-btn').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active').fadeIn();
+
+        $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+
         return false;
     });
-
-
 
 });
