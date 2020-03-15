2c9e293228a5247accde74b426ad2958c97f6e35
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-03-15 21:52:14 +0300

    Test

--- projects/test/dist/assets/js/app.js
+++ projects/test/dist/assets/js/app.js
@@ -3016,6 +3016,59 @@
 
 $(function () {
 
+    $('.program__show-text').click(function () {
+        $(this).toggleClass('active');
+        $(this).prev('.program__text--new').toggleClass('active');
+    });
 
+
+    $('.program__accept-btn').click(function () {
+        var target = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(target).offset().top }, 1000);
+        return false;
+    });
+
+    // Аккордион
+    $('.questions__subtitle').click(function () {
+        $(this).toggleClass('in').next().slideToggle();
+        $('.questions__subtitle').not(this).removeClass('in').next().slideUp();
+    });
+
+
+    // Fixed menu
+    jQuery(window).on("scroll", function () {
+        var scrolled = jQuery(this).scrollTop();
+        if (scrolled > 10) {
+            jQuery('.header').addClass('scrolled');
+        }
+        if (scrolled <= 10) {
+            jQuery('.header').removeClass('scrolled');
+        }
+    });
+
+
+    // Форма в программе лектора
+    $('.popup__lecturer-btn').on('click', function () {
+        $('.popup__form-box').toggleClass('active');
+    });
+
+    $('.modal').on('hide.bs.modal', function () {
+        $('.popup__form-box').removeClass('active');
+    });
+
+
+    // -------- Popup --------
+    $('.program__card-btn, .variants__card-btn').click(function () {
+        var id = $(this).attr('data-id');
+        $('body').addClass('over-hide');
+        $('.popup__wrap').addClass('active');
+        $('.popup').removeClass('active');
+        $('#' + id).addClass('active');
+    });
+
+    $('.popup__close, .popup__overlay').click(function () {
+        $('.popup__wrap').toggleClass('active');
+        $('body').removeClass('over-hide');
+    });
 
 });
