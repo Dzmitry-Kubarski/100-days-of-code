3e724254460e7851bbc2f4dbdc27a93677dd109e
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-03-24 22:21:25 +0300

    Вёрстка, макет №19

--- projects/layout-19-R/src/assets/js/app.js
+++ projects/layout-19-R/src/assets/js/app.js
@@ -12,4 +12,29 @@ $(function () {
         $('.questions__item-header').not(this).removeClass('in').next().slideUp();
     });
 
+
+
+    $('.reviews__sliders').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        speed: 800,
+        adaptiveHeight: true,
+        nextArrow: '<button class="reviews__slider-btn  reviews__slider-btn--prev"><svg><use xlink:href="#svg-prev"></use></svg></button>',
+        prevArrow: '<button class="reviews__slider-btn  reviews__slider-btn--next"><svg><use xlink:href="#svg-next"></use></svg></button>',
+
+
+        // ----- responsive slider -----
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            },
+        ]
+    });
+
 });
