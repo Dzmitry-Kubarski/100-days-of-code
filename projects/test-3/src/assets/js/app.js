c8d2e7335e6b9c305cdc3654e04646a3d6edb588
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-04-03 00:36:36 +0300

    Верстка, test-3

--- projects/test-3/src/assets/js/app.js
+++ projects/test-3/src/assets/js/app.js
@@ -6,6 +6,29 @@
 
 $(function () {
 
+    $('.slides').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 800,
+        // adaptiveHeight: true,
+        // nextArrow: '<button class="reviews__slider-btn  reviews__slider-btn--prev"><svg><use xlink:href="#svg-prev"></use></svg></button>',
+        // prevArrow: '<button class="reviews__slider-btn  reviews__slider-btn--next"><svg><use xlink:href="#svg-next"></use></svg></button>',
 
+
+        // ----- responsive slider -----
+        responsive: [
+            {
+                breakpoint: 646,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    // dots: true,
+                    // arrows: false,
+                }
+            },
+        ]
+    });
 
 });
