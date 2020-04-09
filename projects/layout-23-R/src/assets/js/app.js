75afdc06e00efda3850dbe4ccd6a76eb2792d6c0
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-04-09 18:46:54 +0300

    Верстка, макет №23

--- projects/layout-23-R/src/assets/js/app.js
+++ projects/layout-23-R/src/assets/js/app.js
@@ -3,6 +3,8 @@
 /* Slick slider */
 //= ../../../../../node_modules/slick-carousel/slick/slick.js
 
+//= libs/rangeSlider/ion.rangeSlider.min.js
+
 
 $(function () {
 
@@ -25,15 +27,45 @@ $(function () {
             },
 
         ]
-        // fade: true
-    });
 
+    });
 
     $('.prev').on('click', function (event) {
         $('.intro__slider-items').slick('slickPrev');
     });
     $('.next').on('click', function (event) {
         $('.intro__slider-items').slick('slickNext');
+    });
+
+
+
+
+    // range-slider
+
+    $(".js-range-slider").ionRangeSlider({
+        min: 1,
+        max: 100,
+        from: 31,
+        onStart: function (data) {
+            $('#qw3inp').val($(".js-range-slider").prop("value"));
+        },
+    });
+
+    var my_range = $(".js-range-slider");
+
+    my_range.on("change input", function () {
+        var $inp = $(this);
+        var from2 = $inp.data("from");
+        $('#qw3inp').val(from2);
+
+    });
+
+    var my_range_instance = my_range.data("ionRangeSlider");
+    $('#qw3inp').on('change input', function () {
+        var _val = $(this).val();
+        my_range_instance.update({
+            from: _val,
+        });
     });
 
 });
