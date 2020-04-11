ac14d5508240747d3e3c256cec031bdaf451a82e
Дмитрий Кубарский <55586675+Dzmitry-Kubarski@users.noreply.github.com>
2020-04-11 18:02:34 +0300

    Вёрстка, макет №23

--- projects/layout-23-R/src/assets/js/app.js
+++ projects/layout-23-R/src/assets/js/app.js
@@ -96,4 +96,54 @@ $(function () {
         }
     });
 
+
+
+
+    // step-slider
+
+    $('.step-slider__items').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+
+            {
+                breakpoint: 776,
+                settings: {
+                    slidesToShow: 2,
+                    autoplay: false,
+                }
+            },
+
+            {
+                breakpoint: 567,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+
+        ]
+
+    });
+
+
+    $('.prev').on('click', function (event) {
+        $('.step-slider__items').slick('slickPrev');
+    });
+    $('.next').on('click', function (event) {
+        $('.step-slider__items').slick('slickNext');
+    });
+
+
+
+
+
+    // show consultation seo text
+
+    $('.consultation__seo-btn').on('click', function () {
+        $('.consultation__seo-before').toggleClass('active');
+    });
+
 });
