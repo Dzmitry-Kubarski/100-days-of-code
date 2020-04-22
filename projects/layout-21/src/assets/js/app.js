27d725d7979fbbafa06750d8fcb360e9b3ac367f
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-04-22 19:27:39 +0300

    Вёрстка, макет №21

--- projects/layout-21/src/assets/js/app.js
+++ projects/layout-21/src/assets/js/app.js
@@ -1,10 +1,38 @@
 // ---- Connection libs ----
+// =========================
 
 /* Slick slider */
 //= ../../../../../node_modules/slick-carousel/slick/slick.js
 
+/* Slider Swiper */
+//= ../../../../../node_modules/swiper/js/swiper.min.js
+
 
 $(function () {
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 6,
+        spaceBetween: 30,
+        // grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            900: {
+                slidesPerView: 6,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            576: {
+                slidesPerView: 3,
+            },
+            375: {
+                slidesPerView: 2,
+            },
+        }
+    });
 
 
 
