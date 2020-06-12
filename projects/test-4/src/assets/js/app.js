e2c281f31d412a63471f652c6b4a08905fdc1047
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-06-12 15:37:30 +0300

    Правки test-4

--- projects/test-4/src/assets/js/app.js
+++ projects/test-4/src/assets/js/app.js
@@ -27,42 +27,11 @@ $(function () {
     });
 
 
-    // // ------ Form -------
-    // var btn = document.querySelector('.form__btn');
-    // var inp = document.querySelector('.form__input');
-
-    // function addAlert() {
-    //     if (inp.value !== '') {
-
-    //         var id = $(this).attr('data-id');
-    //         $('body').addClass('over-hide');
-    //         $('.popup__wrap').addClass('active');
-    //         $('.popup').removeClass('active');
-    //         $('#' + id).addClass('active');
-    //     }
-    // }
-
-
-    // $('.popup__close, .popup__overlay').click(function () {
-    //     $('.popup__wrap').toggleClass('active');
-    //     $('body').removeClass('over-hide');
-    // });
-
-
-    // btn.addEventListener('click', addAlert);
-
-
     // ------ Form -------
     $('.form__btn').on('click', function (event) {
 
-
-
-
-
-
         if ($('.form__input').val() !== '') {
             event.preventDefault();
-
 
             var id = $(this).attr('data-id');
             $('body').addClass('over-hide');
@@ -77,25 +46,5 @@ $(function () {
         $('.popup__wrap').toggleClass('active');
         $('body').removeClass('over-hide');
     });
-
-
-
-    // var btn = document.querySelector('.form__btn');
-    // var inp = document.querySelector('.form__input');
-
-    // function addAlert() {
-    //     if (inp.value !== '') {
-
-    //         var id = $(this).attr('data-id');
-    //         $('body').addClass('over-hide');
-    //         $('.popup__wrap').addClass('active');
-    //         $('.popup').removeClass('active');
-    //         $('#' + id).addClass('active');
-    //     }
-    // } 
-
-
-    // btn.addEventListener('click', addAlert);
-
 
 });
