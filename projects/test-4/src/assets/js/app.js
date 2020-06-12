ee91045cac83bb4e97cdaf05cf2a32a9fdde8864
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-06-12 15:36:01 +0300

    правки test-4

--- projects/test-4/src/assets/js/app.js
+++ projects/test-4/src/assets/js/app.js
@@ -27,12 +27,42 @@ $(function () {
     });
 
 
-    // ------ Form -------
-    var btn = document.querySelector('.form__btn');
-    var inp = document.querySelector('.form__input');
+    // // ------ Form -------
+    // var btn = document.querySelector('.form__btn');
+    // var inp = document.querySelector('.form__input');
 
-    function addAlert() {
-        if (inp.value !== '') {
+    // function addAlert() {
+    //     if (inp.value !== '') {
+
+    //         var id = $(this).attr('data-id');
+    //         $('body').addClass('over-hide');
+    //         $('.popup__wrap').addClass('active');
+    //         $('.popup').removeClass('active');
+    //         $('#' + id).addClass('active');
+    //     }
+    // }
+
+
+    // $('.popup__close, .popup__overlay').click(function () {
+    //     $('.popup__wrap').toggleClass('active');
+    //     $('body').removeClass('over-hide');
+    // });
+
+
+    // btn.addEventListener('click', addAlert);
+
+
+    // ------ Form -------
+    $('.form__btn').on('click', function (event) {
+
+
+
+
+
+
+        if ($('.form__input').val() !== '') {
+            event.preventDefault();
+
 
             var id = $(this).attr('data-id');
             $('body').addClass('over-hide');
@@ -40,7 +70,7 @@ $(function () {
             $('.popup').removeClass('active');
             $('#' + id).addClass('active');
         }
-    }
+    });
 
 
     $('.popup__close, .popup__overlay').click(function () {
@@ -49,7 +79,23 @@ $(function () {
     });
 
 
-    btn.addEventListener('click', addAlert);
+
+    // var btn = document.querySelector('.form__btn');
+    // var inp = document.querySelector('.form__input');
+
+    // function addAlert() {
+    //     if (inp.value !== '') {
+
+    //         var id = $(this).attr('data-id');
+    //         $('body').addClass('over-hide');
+    //         $('.popup__wrap').addClass('active');
+    //         $('.popup').removeClass('active');
+    //         $('#' + id).addClass('active');
+    //     }
+    // } 
+
+
+    // btn.addEventListener('click', addAlert);
 
 
 });
