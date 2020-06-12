5bbfe0d1bc1b9ccfe80f99b3034b26702aac0f5d
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2020-06-12 15:09:51 +0300

    Test №4

---
+++ projects/test-4/gulpfile.js
@@ -0,0 +1,166 @@
+"use strict";
+
+const { src, dest } = require("gulp");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const panini = require("panini");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const smartgrid = require('smart-grid');
+
+
+
+/* Paths */
+var path = {
+    build: {
+        html: "dist/",
+        js: "dist/assets/js/",
+        css: "dist/assets/css/",
+        images: "dist/assets/img/",
+        fonts: "dist/assets/fonts/"
+    },
+    src: {
+        html: "src/*.html",
+        js: "src/assets/js/*.js",
+        css: "src/assets/sass/style.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
+        fonts: "src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}"
+
+    },
+    watch: {
+        html: "src/**/*.html",
+        js: "src/assets/js/**/*.js",
+        css: "src/assets/sass/**/*.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
+        fonts: "src/assets/fonts/**/*.{eot,svg,ttf,woff,woff2}",
+    },
+
+    clean: "./dist"
+}
+
+
+
+/* Tasks */
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 3000
+    });
+}
+
+function browserSyncReload(done) {
+    browsersync.reload();
+}
+
+function html() {
+    panini.refresh();
+    return src(path.src.html, { base: "src/" })
+        .pipe(plumber())
+        .pipe(panini({
+            root: 'src/',
+            layouts: 'src/tpl/layouts/',
+            partials: 'src/tpl/partials/',
+            helpers: 'src/tpl/helpers/',
+            data: 'src/tpl/data/'
+        }))
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+}
+
+function css() {
+    return src(path.src.css, { base: "src/assets/sass/" })
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            Browserslist: ['> 0.1%'],
+            cascade: false
+        }))
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
+}
+
+function js() {
+    return src(path.src.js, { base: './src/assets/js/' })
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
+}
+
+function images() {
+    return src(path.src.images)
+        .pipe(dest(path.build.images));
+}
+
+function fonts() {
+    return src(path.src.fonts)
+        .pipe(dest(path.build.fonts));
+}
+
+function clean() {
+    return del(path.clean);
+}
+
+function watchFiles() {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.images], images);
+    gulp.watch('./smartgrid.js', grid);
+}
+
+
+function grid(done) {
+    delete require.cache[require.resolve('./smartgrid.js')];
+    let settings = require('./smartgrid.js');
+    smartgrid('src/assets/sass', settings);
+
+    settings.offset = '3.1%';
+    settings.filename = 'smart-grid-per';
+    smartgrid('src/assets/sass', settings);
+
+    done();
+}
+
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+
+/* Exports Tasks */
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
+exports.grid = grid;
+exports.fonts = fonts;
+
---
+++ projects/test-4/src/assets/js/app.js
@@ -0,0 +1,55 @@
+
+
+
+$(function () {
+
+
+    // ------------ Tabs Slider -------
+    $('.dots__btn').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.slider__item').removeClass('active').hide();
+        $('.dots__btn').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active').fadeIn();
+        return false;
+    });
+
+
+
+    // ------------ Tabs Calc-item -------
+    $('.calculator__dots-btn').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.calculator-item').removeClass('active').hide();
+        $('.calculator__dots-btn').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active').fadeIn();
+        return false;
+    });
+
+
+    // ------ Form -------
+    var btn = document.querySelector('.form__btn');
+    var inp = document.querySelector('.form__input');
+
+    function addAlert() {
+        if (inp.value !== '') {
+
+            var id = $(this).attr('data-id');
+            $('body').addClass('over-hide');
+            $('.popup__wrap').addClass('active');
+            $('.popup').removeClass('active');
+            $('#' + id).addClass('active');
+        }
+    }
+
+
+    $('.popup__close, .popup__overlay').click(function () {
+        $('.popup__wrap').toggleClass('active');
+        $('body').removeClass('over-hide');
+    });
+
+
+    btn.addEventListener('click', addAlert);
+
+
+});
